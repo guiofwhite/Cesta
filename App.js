1016586dc71c0fba0f759ae33ce4fbcd5050338f
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2026-02-09 12:42:05 +0000

    a

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import Sobrenome from './src /Telas/Sobrenome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Guilherme Marinho!</Text>
+      <Text>Guilherme Marinho</Text>
+      <Sobrenome />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src /Telas/Sobrenome.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {Text} from "react-native"
+
+export default function Sobrenome(){
+    return <Text> da Silva </Text>
+}
